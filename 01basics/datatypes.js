8dc5021718fff7839650442f42a01cb6240766aa
arshu02 <141254512+arshu02@users.noreply.github.com>
2024-01-28 15:14:46 +0000

    variables and datatypes

---
+++ 01basics/datatypes.js
@@ -0,0 +1,9 @@
+"use strict";//treat all js code as newer version
+//number=> 2 to power 53
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined =>
+//symbol => unique
+//object
---
+++ 01basics/variables.js
@@ -0,0 +1,9 @@
+const accountId=456523
+let accountEmail ="lalu@gmail.com"
+accountcity ="ranchi"
+ 
+accountPassword="25362"
+
+ console.table([accountId,accountEmail,accountPassword,accountcity])
+
+
